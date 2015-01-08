f7507e5f885b11b1c1dd2614830f06c7d09f031f
Kyle Kellogg <kyle@kylekellogg.com>
2015-01-08 08:15:11 -0500

    Updated main.js to make use of new page modules

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 (function() {
   'use strict';
 
+  var $ = require( 'jquery' );
+  var page = require( 'page' );
+  var index = require( './index.js' );
+  var race = require( './choose-race.js' );
+  var clss = require( './choose-class.js' );
+  var abilities = require( './determine-abilities.js' );
+  var describe = require( './describe-character.js' );
+  var equipment = require( './choose-equipment.js' );
+  // var notfound = require( './notfound.js' );
+
   $( document ).ready( function onReady() {
-    console.log( 'ready' );
+
+    page( '/', index );
+    page( '/choose-race', race );
+    page( '/choose-class', clss );
+    page( '/determine-abilities', abilities );
+    page( '/describe-character', describe );
+    page( '/choose-equipment', equipment );
+    // page( '*', notfound );
+    page( {
+      hashbang: true
+    } );
+
   } );
 })();
