1497ad04d84d545f50348bfb9e3890b4fe1c9704
Kyle Kellogg <kyle@kylekellogg.com>
2015-01-08 20:32:59 -0500

    Adding JS changes for choose-race and template for it

--- js/choose-race.js
+++ js/choose-race.js
@@ -1,23 +1,10 @@
 module.exports = function(ctx, next) {
   'use strict';
-  console.log( 'choose race page' );
+  var $ = require( 'jquery' );
+
   ctx.handled = true;
   // ctx.save();
 
-  var $ = require( 'jquery' );
-  var Handlebars = require( 'handlebars' );
-  var helpers = require( './template.helpers.js' )( Handlebars );
-  var templates = require( './templates.js' );
-
-  $.getJSON( '/data/races.json', function onDataLoaded( data ) {
-      console.log( 'loaded', data );
-      $( 'main' ).html( Handlebars.templates.race( data ) );
-    } )
-    .done( function onDataDone() {
-      console.log( 'getJSON done' );
-    } )
-    .fail( function onDataFail() {
-      console.log( 'getJSON fail' );
-    } )
-    .always( next );
+  //  Page specific
+  next();
 };
