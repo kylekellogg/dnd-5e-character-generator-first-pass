c068d2b54175e8bf8b7ad45cd8f92a395aaedba0
Kyle Kellogg <kyle@kylekellogg.com>
2015-01-08 20:35:58 -0500

    Name change for choose-race template helper to generate good ID

--- js/template.helpers.js
+++ js/template.helpers.js
@@ -1,8 +1,7 @@
 module.exports = function( Handlebars ) {
   'use strict';
 
-  Handlebars.registerHelper( 'race-formatID', function(options) {
-    console.log( 'got', this, 'will return', (''+this.name).trim().toLowerCase().replace( /\s/g, '_' ) );
+  Handlebars.registerHelper( 'choose-race-formatID', function(options) {
     return (''+this.name).trim().toLowerCase().replace( /\s/g, '_' );
   } );
 };
