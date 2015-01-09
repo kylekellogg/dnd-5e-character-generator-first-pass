240ef80340ec904c4219d7b45a305ea45fb53c88
Kyle Kellogg <kyle@kylekellogg.com>
2015-01-08 22:09:25 -0500

    Got state scenario passing, created new scenario for state change on race select

--- features/choose_race.feature
+++ features/choose_race.feature
@@ -4,6 +4,12 @@ Feature: Choose Race
   So that I can specify what character I'd like to play
 
   Scenario: A User is presented with Race options
-    Given I am on the homepage
+    Given I am on "/choose-race"
     When the page has loaded
     Then the title should be "Choose a Race"
+
+  Scenario: A User selects a Race to learn more
+    Given I am on "/choose-race"
+    When the page has loaded
+    And I have clicked "label" with text "Hill Dwarf"
+    Then I should see "Dwarven Toughness" in "#features"
--- features/step_definitions/choose_race_steps.js
+++ features/step_definitions/choose_race_steps.js
@@ -1,8 +1,19 @@
 module.exports = function chooseRaceSteps() {
+  'use strict';
+
+  var assert = require( 'assert' );
+  var $ = require('jquery');
+
   this.World = require("../support/world.js").World;
 
-  this.Given( /^I am on the homepage$/, function( callback ) {
-    this.visit( 'http://localhost:3000/', callback );
+  this.Given( /^I am on "([^"]*)"$/, function( page, callback ) {
+    var base = 'http://loc.al/';
+
+    if ( page.length > 0 ) {
+      this.visit( base + '#!' + page, callback );
+    } else {
+      this.visit( base, callback );
+    }
   } );
 
   this.When( /^the page has loaded$/, function( callback ) {
@@ -16,10 +27,45 @@ module.exports = function chooseRaceSteps() {
 
   this.Then( /^the title should be "([^"]*)"$/, function( text, callback ) {
     try {
-      this.browser.assert.text( 'title', text );
+      assert.equal( this.browser.document.title, text );
       callback();
     } catch ( e ) {
-      callback.fail( 'Expected to find ' + text + ' as <title> content' );
+      callback.fail( 'Expected to find ' + text + ' as <title> content, but found ' + this.browser.document.title + ' instead' );
     }
   } );
+
+  this.When(/^I have clicked "([^"]*)" with text "([^"]*)"$/, function ( el, text, callback) {
+    try {
+      var win = this.browser.window;
+      $(win);
+      var $el = win.$( el + ':contains(' + text + ')' );
+
+      if ( $el.length > 0 ) {
+        assert.equal( $el.text(), text );
+        $el.click();
+        callback();
+      } else {
+        callback.fail( 'Could not find an element (' + el + ') with text ' + text );
+      }
+    } catch ( e ) {
+      callback.fail( 'Expected to find element ' + el + ' with text content ' + text );
+    }
+  });
+
+  this.Then(/^I should see "([^"]*)" in "([^"]*)"$/, function ( text, el, callback) {
+    try {
+      var win = this.browser.window;
+      $(win);
+      var $el = win.$( el + ':contains(' + text + ')' );
+
+      if ( $el.length > 0 ) {
+        assert.equal( $el.text(), text );
+        callback();
+      } else {
+        callback.fail( 'Could not find any elements to match ' + el );
+      }
+    } catch ( e ) {
+      callback.fail( 'Expected to find element ' + el + ' with text content ' + text );
+    }
+  });
 };
